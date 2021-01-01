b6d00a17d6993d28f6bab695f777ab40ebaa8a21
Tymoteusz Dzienniak <dzienniak.tymoteusz@gmail.com>
2021-01-01 23:43:39 +0100

    Problem 1 part 2 solved in F#

--- F#/src/1/Program.fs
+++ F#/src/1/Program.fs
@@ -1,24 +1,43 @@
+let rec makeCombinationsWithLength length list =
+  match length with
+    | 1 ->
+      list
+      |> List.map (fun i -> [i])
+    | _ ->
+      match list with
+        | [] -> []
+        | first::rest ->
+          rest
+          |> makeCombinationsWithLength (length - 1)
+          |> List.map (fun i -> List.append [first] i)
+          |> List.append (makeCombinationsWithLength length rest)
+
+let makeTripples list = makeCombinationsWithLength 3 list
+let makePairs list = makeCombinationsWithLength 2 list
+let findSolution combinations =
+  combinations
+  |> List.filter (fun i -> List.sum i = 2020) 
+  |> List.map (fun i -> List.fold (( * )) 1 i)
+  |> List.head
+
+let Part1 input =
+  input
+  |> makePairs
+  |> findSolution
+
+let Part2 input =
+  input
+  |> makeTripples
+  |> findSolution
+
 [<EntryPoint>]
 let main argv =
-  let lines = System.IO.File.ReadLines "input.txt"
-
-  let rec makePairs list =
-    match list with
-      | [] -> []
-      | first::rest ->
-        rest
-        |> List.map (fun i -> (first, i))
-        |> List.append (makePairs rest)
-
-  let result =
-    lines
+  let lines =
+    System.IO.File.ReadLines "input.txt"
     |> List.ofSeq
     |> List.map int
-    |> makePairs
-    |> List.filter (fun (a, b) -> a + b = 2020) 
-    |> List.map (fun (a, b) -> a * b)
-    |> List.head
   
-  System.Console.WriteLine result
+  printfn "Part 1: %d" (Part1 lines)
+  printfn "Part 2: %d" (Part2 lines)
 
   0
