cd0e59c6c1549ae2e4d1b394bec450029823acdf
Soanvig <soanvig@gmail.com>
2021-01-05 00:51:15 +0100

    haskel/task 2 using parsec

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -1,2 +1,6 @@
-import Task1 hiding (main)
-import Task2 hiding (main)
+module Main where
+
+  import Task1 hiding (main)
+  import Task2 hiding (main)
+
+  main = print "Import task directly"
--- haskell/src/Task2.hs
+++ haskell/src/Task2.hs
@@ -1,27 +1,44 @@
 module Task2 where
 
-  import Helpers (readLinesfromFile, countInString)
-  import Data.Char (digitToInt)
+  import Helpers (readLinesfromFile, countInString, toInt)
 
-  type Schema = ((Int, Int), Char, String)
+  import Text.Parsec
+    ( anyChar,
+      char,
+      digit,
+      letter,
+      spaces,
+      many1,
+      many,
+      parse,
+      ParseError,
+      Parsec )
+  import Control.Monad.Identity (Identity)
+  import Control.Exception
 
-  wordsToSchema :: [String] -> Schema
-  wordsToSchema [range, charDef, password] =
-    let char = head charDef
-        [min, '-', max] = range
-    in ((digitToInt min, digitToInt max), char, password) 
+  type ParseResult = (String, String, Char, String)
 
-  verifySchema :: Schema -> Bool
-  verifySchema ((min, max), char, password) =
+  lineParser :: Parsec String () ParseResult
+  lineParser = do
+    min <- many1 digit
+    char '-'
+    max <- many1 digit
+    spaces
+    ltr <- letter
+    char ':'
+    spaces
+    password <- many anyChar
+    return (min, max, ltr, password)
+
+  parseLine :: String -> Either ParseError ParseResult
+  parseLine = parse lineParser ""
+
+  verifyResult :: Either ParseError ParseResult -> Bool
+  verifyResult (Left _) = False
+  verifyResult (Right (min, max, char, password)) = 
     let count = countInString char password
-    in count >= min && count <= max
-
-  passwordFromSchema :: Schema -> String
-  passwordFromSchema (_, _, password) = password
+    in count >= toInt min && count <= toInt max
       
   main = do
     input <- readLinesfromFile "./data/2.txt"
-    print
-      $ map passwordFromSchema
-      $ filter verifySchema
-      $ map (wordsToSchema . words) input
+    print $ (length . (filter verifyResult . map parseLine)) input
